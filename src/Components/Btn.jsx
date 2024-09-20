20dce543b72ffac60064929f8237e037ff9b791f
Arshjeevs <harshan4648@gmail.com>
2024-09-20 19:57:54 +0530

    ADD: Frontend and Rendering Added

---
+++ src/Components/Btn.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Btn = ({name}) => {
+  return (
+    <button
+      type="button"
+      className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    >
+      {name}
+      <svg
+          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+    </button>
+  );
+};
+
+export default Btn;
